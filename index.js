50fba50a51eaa63f6a0fd1368c16bc84dbbcb5f4
Panu Horsmalahti <panu.horsmalahti@iki.fi>
2015-05-24 23:32:14 +0300

    Implement deep size

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-var SizeOf = require('bindings')('SizeOf');
-module.exports = SizeOf;
+var SizeOfNative = require('bindings')('SizeOf');
+module.exports = SizeOfNative;
 
-// from lodash
-function isObject(value) {
-  var type = typeof value;
-  return !!value && (type == 'object' || type == 'function');
+var sizeOf = module.exports.sizeOf;
+
+/**
+ * Sum of a and b.
+ * @param {number} a
+ * @param {number} b
+ * @returns {numer} sum of a and b
+ */
+function sum(a, b) {
+  return a + b;
 }
 
-module.exports.sizeOfDeep = function(value) {
-  if (!isObject(value)) {
+/**
+ * Calculate the size of the object including all references recursively.
+ * @param {any} value
+ * @returns {number} The size of the value and the sizes of it's references.
+ */
+function sizeOfDeep(value) {
+  if (value && typeof value === "object" && arguments.length === 1) {
+    var size = sizeOf(value);
+    return size + Object.keys(value)
+                        .map(function (key) {
+                          return sizeOfDeep(value[key]);
+                        })
+                        .reduce(sum);
+  } else {
     var args = Array.prototype.slice.call(arguments);
     return module.exports.sizeOf.apply(this, args);
-  } else {
-    // TODO
-    return 0;
   }
 }
+
+module.exports.sizeOfDeep = sizeOfDeep;
--- test/test.js
+++ test/test.js
@@ -1,16 +1,16 @@
-var sizeOf = require('../index.js');
-var assert = require('assert');
+var sizeOf = require("../index.js");
+var assert = require("assert");
 
-describe('native extension', function() {
-  it('should export function that returns a number', function() {
-    assert.equal(typeof sizeOf.sizeOf(), 'number');
+describe("native extension", function() {
+  it("should export function that returns a number", function() {
+    assert.equal(typeof sizeOf.sizeOf(), "number");
   });
 
-  it('should return a size 0 for no arguments', function() {
+  it("should return a size 0 for no arguments", function() {
     assert.equal(sizeOf.sizeOf(), 0);
   });
 
-  it('should throw an error for more than 1 argument', function() {
+  it("should throw an error for more than 1 argument", function() {
     var throwed = false;
     try {
       sizeOf.sizeOf(0, 1);
@@ -21,16 +21,28 @@ describe('native extension', function() {
     assert(throwed);
   });
 
-  it('should return a size > 0 for a number', function() {
+  it("should return a size > 0 for a number", function() {
     assert(sizeOf.sizeOf(0) > 0);
   });
 
+  it("should return a size > 0 for a boolean", function() {
+    assert(sizeOf.sizeOf(true) > 0);
+  });
+
+  it("should return a size > 0 for an empty string", function() {
+    assert(sizeOf.sizeOf("") > 0);
+  });
+
+  it("should return a size > 0 for a string", function() {
+    assert(sizeOf.sizeOf("abc") > 0);
+  });
+
   describe("sizeOfDeep", function() {
-    it('should return a size 0 for no arguments', function() {
+    it("should return a size 0 for no arguments", function() {
       assert.equal(sizeOf.sizeOfDeep(), 0);
     });
 
-    it('should throw an error for more than 1 argument', function() {
+    it("should throw an error for more than 1 argument", function() {
       var throwed = false;
       try {
         sizeOf.sizeOfDeep(0, 1);
@@ -41,8 +53,20 @@ describe('native extension', function() {
       assert(throwed);
     });
 
-    it('should return a size > 0 for a number', function() {
+    it("should return a size > 0 for a number", function() {
       assert(sizeOf.sizeOfDeep(0) > 0);
+    });
+
+    it("should return a size > 0 for a boolean", function() {
+      assert(sizeOf.sizeOfDeep(true) > 0);
+    });
+
+    it("should return a size > 0 for an empty string", function() {
+      assert(sizeOf.sizeOfDeep("") > 0);
+    });
+
+    it("should return a size > 0 for a string", function() {
+      assert(sizeOf.sizeOfDeep("abc") > 0);
     });
   })
 });
